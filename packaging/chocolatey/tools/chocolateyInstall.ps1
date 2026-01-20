ffa3a49449d17d71a88f9d93de7c278a681b9df2
andreisuslov <truvord@gmail.com>
2026-01-19 20:24:43 -0500

    Add CI/CD and package manager support
    - Add MIT LICENSE file
    - Add GitHub Actions CI workflow for testing
    - Add GitHub Actions release workflow for automated releases
    - Add Chocolatey packaging files
    - Update README with Homebrew and Chocolatey installation instructions

---
+++ packaging/chocolatey/tools/chocolateyInstall.ps1
@@ -0,0 +1,18 @@
+$ErrorActionPreference = 'Stop'
+
+$packageName   = 'connecto'
+$version       = '0.1.0'
+$toolsDir      = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url64         = "https://github.com/andreisuslov/ssh/releases/download/v$version/connecto-windows-x86_64.zip"
+$checksum      = 'PLACEHOLDER_CHECKSUM'
+$checksumType  = 'sha256'
+
+$packageArgs = @{
+    packageName    = $packageName
+    unzipLocation  = $toolsDir
+    url64bit       = $url64
+    checksum64     = $checksum
+    checksumType64 = $checksumType
+}
+
+Install-ChocolateyZipPackage @packageArgs
